f81658127d2a738993bb6dbcc4e3fe6cf1945c50
Ashish Patel <ashpat86@gmail.com>
2024-03-27 18:32:45 -0400

    Initial Upload

---
+++ FacadePattern/src/jdp2e/facade/demo/FacadePatternExample.java
@@ -0,0 +1,18 @@
+package jdp2e.facade.demo;
+
+// Client Code
+public class FacadePatternExample
+{
+    public static void main(String[] args) {
+        System.out.println("***Facade Pattern Demo***\n");
+        // Creating Robots
+        RobotFacade milanoRobotFacade = new RobotFacade();
+        milanoRobotFacade.constructMilanoRobot();
+        RobotFacade robonautRobotFacade = new RobotFacade();
+        robonautRobotFacade.constructRobonautRobot();
+        // Destroying robots
+        milanoRobotFacade.destroyMilanoRobot();
+        robonautRobotFacade.destroyMilanoRobot();
+
+    }
+}
---
+++ FacadePattern/src/jdp2e/facade/demo/RobotColor.java
@@ -0,0 +1,26 @@
+package jdp2e.facade.demo;
+
+public class RobotColor
+{
+    public void setDefaultColor()
+    {
+        System.out.println(" This is steel color robot.");
+    }
+
+    public void setGreenColor()
+    {
+        System.out.println(" This is a green color robot.");
+    }
+
+    public void resetMilanoHands()
+    {
+        System.out.println(" EH1 Milano hands are about to be destroyed.");
+    }
+
+    public void resetRobonautHands()
+    {
+        System.out.println(" Robonaut hands are about to be destroyed.");
+    }
+
+
+}
